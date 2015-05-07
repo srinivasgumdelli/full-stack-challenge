2f1d01194d575aea2d72ba91499277c6bb327cde
Srinivas Gumdelli <sgumdelli@gmail.com>
2015-05-07 01:41:42 -0500

    Some Cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -16,24 +16,6 @@ ActiveRecord::Schema.define(version: 20150426011042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",               default: "", null: false
-    t.string   "encrypted_password",  default: "", null: false
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.integer  "failed_attempts",     default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-
   create_table "eligibilities", force: :cascade do |t|
     t.string   "eligibility"
     t.string   "description"
